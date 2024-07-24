34fee5d8a0c005bdc7cdd7d73ba5d3a476e15ba3
IlyaZolotuhin <1zolotyhin1@gmail.com>
2024-07-24 21:23:51 +0700

    product.service.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,27 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {NgForOf, TitleCasePipe} from "@angular/common";
+import {AsyncPipe, NgForOf, TitleCasePipe} from "@angular/common";
 import {ProductComponent} from "../app/components/product/product.component";
 import {IProduct} from "./models/product";
-import {products as data} from "./data/products";
-
+import {ProductService} from "./services/product.service";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TitleCasePipe, ProductComponent, NgForOf],
+  imports: [RouterOutlet, TitleCasePipe, ProductComponent, NgForOf, AsyncPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'angular project';
 
-  products: IProduct[] = data
+  products: IProduct[] = []
+  constructor(private productsService: ProductService) {
+  }
+
+  ngOnInit(): void {
+    this.productsService.getAll().subscribe(products => {
+      console.log(products)
+    })
+  }
 }
