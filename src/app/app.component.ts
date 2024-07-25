ffc9632af6225a1ab473e4d1a01b6d6379715957
IlyaZolotuhin <1zolotyhin1@gmail.com>
2024-07-25 21:11:18 +0700

    product.service.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,35 @@
 import {Component, OnInit} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {AsyncPipe, NgForOf, TitleCasePipe} from "@angular/common";
+import {AsyncPipe, NgForOf, NgIf, TitleCasePipe} from "@angular/common";
 import {ProductComponent} from "../app/components/product/product.component";
 import {IProduct} from "./models/product";
 import {ProductService} from "./services/product.service";
+import {Observable, tap} from "rxjs";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TitleCasePipe, ProductComponent, NgForOf, AsyncPipe],
+  imports: [RouterOutlet, TitleCasePipe, ProductComponent, NgForOf, AsyncPipe, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent implements OnInit{
   title = 'angular project';
+  /*products: IProduct[] = []*/
+  loading = false
+  products$: Observable<IProduct[]>
 
-  products: IProduct[] = []
   constructor(private productsService: ProductService) {
   }
 
   ngOnInit(): void {
-    this.productsService.getAll().subscribe(products => {
-      console.log(products)
-    })
+    this.loading = true
+    this.products$ = this.productsService.getAll().pipe(
+      tap(() => this.loading = false)
+    )
+    /*this.productsService.getAll().subscribe(products => {
+      this.products = products
+      this.loading = false
+    })*/
   }
 }
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,5 +1,7 @@
 import {Injectable} from "@angular/core";
-import {HttpClient} from "@angular/common/http";
+import {HttpClient, HttpErrorResponse, HttpParams} from "@angular/common/http";
+import {catchError, delay, Observable, throwError} from "rxjs";
+import {IProduct} from "../models/product";
 
 @Injectable({
   providedIn: 'root'
@@ -9,8 +11,19 @@ export class ProductService {
 constructor(private  http: HttpClient) {
 }
 
-  getAll() {
-    return this.http.get('https://fakestoreapi.com/products')
+  getAll(): Observable<IProduct[]> {
+    return this.http.get<IProduct[]>('https://fakestoreapi.com/products1',{
+      params: new HttpParams({
+        fromObject: {limit: 5}
+      })
+    }).pipe(
+      delay(2000)/*pipe чтоб замедлить загрузку на 2 сек*/,
+      catchError(this.errorHandler)
+    )
+  }
+
+  private errorHandler(error: HttpErrorResponse) {
+return throwError(() => error.message)
   }
 }
 
