4d197adb7f154414c51579136f292e0078f36bbe
IlyaZolotuhin <1zolotyhin1@gmail.com>
2024-07-30 21:11:31 +0700

    error.service.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,11 +5,12 @@ import {ProductComponent} from "../app/components/product/product.component";
 import {IProduct} from "./models/product";
 import {ProductService} from "./services/product.service";
 import {Observable, tap} from "rxjs";
+import {GlobalErrorComponent} from "../app/components/global-error/global-error.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TitleCasePipe, ProductComponent, NgForOf, AsyncPipe, NgIf],
+  imports: [RouterOutlet, TitleCasePipe, ProductComponent, NgForOf, AsyncPipe, NgIf, GlobalErrorComponent, GlobalErrorComponent, GlobalErrorComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,29 +1,35 @@
 import {Injectable} from "@angular/core";
 import {HttpClient, HttpErrorResponse, HttpParams} from "@angular/common/http";
-import {catchError, delay, Observable, throwError} from "rxjs";
+import {catchError, delay, Observable, retry, throwError} from "rxjs";
 import {IProduct} from "../models/product";
+import {ErrorService} from "./error.service";
 
 @Injectable({
   providedIn: 'root'
 })
 
 export class ProductService {
-constructor(private  http: HttpClient) {
-}
+  constructor(
+    private http: HttpClient,
+  private errorService: ErrorService
+  ) {
+  }
 
   getAll(): Observable<IProduct[]> {
-    return this.http.get<IProduct[]>('https://fakestoreapi.com/products1',{
+    return this.http.get<IProduct[]>('https://fakestoreapi.com/products', {
       params: new HttpParams({
         fromObject: {limit: 5}
       })
     }).pipe(
-      delay(2000)/*pipe чтоб замедлить загрузку на 2 сек*/,
-      catchError(this.errorHandler)
+      delay(200)/*pipe чтоб замедлить загрузку на 2 сек*/,
+      retry(2)/*запрос будет отправлен дважды*/,
+      catchError(this.errorHandler.bind(this))
     )
   }
 
   private errorHandler(error: HttpErrorResponse) {
-return throwError(() => error.message)
+    this.errorService.handle(error.message)
+    return throwError(() => error.message)
   }
 }
 
