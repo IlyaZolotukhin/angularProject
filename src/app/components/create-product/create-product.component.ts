4db8b56b22ad54ecd05a7bc684dd83cdfdd31d2e
IlyaZolotuhin <1zolotyhin1@gmail.com>
2024-08-05 21:33:34 +0700

    modal continued

--- src/app/components/create-product/create-product.component.ts
+++ src/app/components/create-product/create-product.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
-import {FormControl, FormGroup, ReactiveFormsModule} from "@angular/forms";
+import {FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
+import {NgIf} from "@angular/common";
+import {FocusDirective} from "../../directives/focus.directive";
 
 @Component({
   selector: 'app-create-product',
   standalone: true,
   imports: [
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    NgIf,
+    FocusDirective,
+    FocusDirective
   ],
   templateUrl: './create-product.component.html',
   styleUrl: './create-product.component.css'
@@ -13,8 +18,15 @@ import {FormControl, FormGroup, ReactiveFormsModule} from "@angular/forms";
 export class CreateProductComponent {
 
   form = new FormGroup({
-    title: new FormControl<string>('')
+    title: new FormControl<string>('', [
+      Validators.required,
+      Validators.minLength(6)
+    ])
   })
+
+  get title() {
+    return this.form.controls.title as FormControl
+  }
 
   submit() {
     console.log(this.form.value)
